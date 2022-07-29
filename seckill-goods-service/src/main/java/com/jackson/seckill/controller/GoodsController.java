5d3e090b1a469964716c282504f2ce6984bce55d
yuhuajie <yuhuajie0810@gmail.com>
2022-07-29 11:35:15 +0800

    new increase files

---
+++ seckill-goods-service/src/main/java/com/jackson/seckill/controller/GoodsController.java
@@ -0,0 +1,18 @@
+package com.jackson.seckill.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * ClassName: GoodsController
+ * Package: com.jackson.seckill.controller
+ * Description:
+ *
+ * @Date: 7/29/2022 11:20 AM
+ * @Author: JacksonYu
+ */
+@Controller
+public class GoodsController {
+
+
+
+}
